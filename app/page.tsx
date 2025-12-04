3f968c506e06138c23d21f15041e4058c6277991
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-12-04 23:56:10 +0530

    layout making

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to taapke!</h1>
+      <h1 className="text-9xl text-red-700">Welcome to taapke!</h1>
     </div>
   );
 }
