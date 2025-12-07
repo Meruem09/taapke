2921b758a8efb4674db4dc8e19f65cfaf177abe6
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-12-07 23:49:12 +0530

    made  component button2

--- app/components/buttons/button2.tsx
+++ app/components/buttons/button2.tsx
@@ -8,7 +8,7 @@ export default function Button2({ btn_name, onClick }: MyButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="box-border border border-black text-4xl px-8 py-5 text-orange-600 cursor-pointer"
+      className="box-border border border-black text-4xl px-8 py-5 text-orange-600 cursor-pointer   "
     >
       {btn_name}
     </button>
